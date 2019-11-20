cea0d47a73009e0f4658b0687194a99c37ecea98
David Mans <me@davidmans.io>
2019-11-20 14:47:56 -0600

    Initial Commit
    Added index, library, and support files

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * This file is part of Message-Authenticator.
+ * 
+ * Message-Authenticator is free library: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Message-Authenticator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @author David Mans, https://github.com/mansdj
+ * @license GNU GPL see license.txt
+ * 
+ */
+module.exports = require('./lib/MessageAuthenticator')
